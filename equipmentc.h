1933ee501edfde57b2548b5a71cefb16bc1869b3
sma56 <sma56@zips.uakron.edu>
2015-05-07 11:51:02 -0400

    adding equipment class header

---
+++ equipmentc.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   equipmentc.h
+ * Author: Scott
+ *
+ * Created on May 6, 2015, 7:37 PM
+ */
+
+#ifndef EQUIPMENTC_H
+#define	EQUIPMENTC_H
+
+class equipment
+{
+private:
+    equipment();
+    equipment(char equipped);
+    void weapon();
+    void armor();
+    void ring();
+    void scroll();
+    void potions();
+    int gold();
+};
+
+#endif	/* EQUIPMENTC_H */
+
